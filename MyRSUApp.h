18319461a58a85f3f9dd6899751a8ddba8ca1107
shemalperera <shemal.perera.sp@gmail.com>
2025-12-10 18:49:19 +0530

    Battery cap. Memory cap params of vehicle and static params of RSU.

--- MyRSUApp.h
+++ MyRSUApp.h
@@ -24,6 +24,12 @@ private:
     RSUHttpPoster poster{"http://127.0.0.1:8000/ingest"};
     // self-message used for periodic beacons; keep as member so we can cancel/delete safely
     omnetpp::cMessage* beaconMsg{nullptr};
+    
+    // Edge server resource parameters (static values)
+    double edgeCPU_GHz = 0.0;           // Edge server CPU capacity (GHz)
+    double edgeMemory_GB = 0.0;         // Edge server memory capacity (GB)
+    int maxVehicles = 0;                // Maximum vehicles this RSU can serve
+    double processingDelay_ms = 0.0;    // Base processing delay (ms)
 };
 
 } // namespace complex_network
